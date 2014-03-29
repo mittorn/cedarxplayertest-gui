7677edf63ba32e1802c5ff7ebc6743b84e637141
mittorn <mittorn@sibmail.com>
2014-03-30 00:16:41 +0700

    Keep aspect ratio; Add fullscreen switch on f; Solve some warnings

--- cedarx-gui.c
+++ cedarx-gui.c
@@ -1,60 +1,80 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
 #include <pthread.h>
 #include <sunxi_disp_ioctl.h>
 #include <X11/Xlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+Display *dis;
+Window win;
 __disp_layer_info_t layer, layer0;
-int disp, width, height, args[4]={0,100,(int)&layer0,0};
-FILE * pipe;
+int disp, width, height, args[4]={0,100,(int)&layer0,0}, fs=0;
+FILE * cpt;
 void keyevent(int k)
 {
-	char c;
-	if(k<127)c=k;
-	else
+	char c=k;
 	switch(k)
 	{
+		case 0x66:XFlush(dis);fs=!fs;break;
 		case 0xFF51:c='j';break;
 		case 0xFF53:c='l';break;
 		case 0xFF1B:c='q';break;
 	}
-	fprintf(pipe,"%c",c);
+	fprintf(cpt,"%c",c);
 	fprintf(stderr, "KeyEvent: %c 0x%X\n", c, k);
-	fflush(pipe);
+	fflush(cpt);
 }
 static void * eventthread()
 {
-	Display *dis=XOpenDisplay(0);
+	dis=XOpenDisplay(0);
 	Atom XWMDeleteMessage = XInternAtom(dis, "WM_DELETE_WINDOW", False);
-
 	XSetWindowAttributes  swa;
 	swa.event_mask  =  ExposureMask | ButtonMotionMask | Button1MotionMask |
 		ButtonPressMask | ButtonReleaseMask| StructureNotifyMask| KeyPressMask;
 	Window win  =  XCreateWindow (dis, DefaultRootWindow(dis),0, 0, width ,
 		height, 0, CopyFromParent, InputOutput, CopyFromParent, 
-		CWEventMask, &swa );
+		CWEventMask | CWOverrideRedirect | CWBorderPixel | CWBackPixel, &swa );
 	XStoreName(dis,win,"CedarXPlayerTest");
 	XSetWMProtocols(dis, win, &XWMDeleteMessage, 1);
 	XSetWindowBackground(dis, win, 0x000102);
 	XMapWindow(dis,win);
 	XFlush(dis);
-	XEvent ev;
+	XEvent ev, cev;
+	memset(&cev,0,sizeof(XEvent));
+	cev.type = ClientMessage;
+	cev.xclient.type = ClientMessage;
+	cev.xclient.send_event = 1;
+	cev.xclient.window = win;
+	cev.xclient.message_type = XInternAtom( dis, "_NET_WM_STATE", 0);
+	cev.xclient.format = 32;
+	cev.xclient.data.l[0] = 0;
+	cev.xclient.data.l[1] = XInternAtom( dis, "_NET_WM_STATE_FULLSCREEN", 0);
+	int tmp;
 	while(1)
 	{
 		XNextEvent(dis,&ev);
 		switch(ev.type)
 		{
 			case ConfigureNotify:
-				layer.scn_win.x=(float)ev.xconfigure.x*(float)layer0.scn_win.width/(float)layer0.src_win.width-layer0.src_win.x+layer0.scn_win.x;
-				layer.scn_win.y=(float)ev.xconfigure.y*(float)layer0.scn_win.height/(float)layer0.src_win.height-layer0.src_win.y+layer0.scn_win.y;
-				layer.scn_win.width=((int)ev.xconfigure.width)*layer0.scn_win.width/layer0.src_win.width;
-				layer.scn_win.height=((int)ev.xconfigure.height)*layer0.scn_win.height/layer0.src_win.height;
-				fprintf(stderr,"%d %d %d %d %d %d\n", ev.xconfigure.x*layer0.scn_win.width/(float)layer0.src_win.width, ev.xconfigure.y, layer.scn_win.x, layer.scn_win.y, layer.scn_win.width, layer.scn_win.height);
-				ioctl(disp,DISP_CMD_LAYER_SET_PARA,args);
+				width=ev.xconfigure.width;
+				height=ev.xconfigure.height;
+				if((tmp=ev.xconfigure.width*layer.src_win.height/layer.src_win.width)<ev.xconfigure.height)height=tmp;
+				else width=ev.xconfigure.height*layer.src_win.width/layer.src_win.height;
+				layer.scn_win.x=(float)ev.xconfigure.x*(float)layer0.scn_win.width/(float)layer0.src_win.width-layer0.src_win.x+layer0.scn_win.x+(ev.xconfigure.width-width)/2;
+				layer.scn_win.y=(float)ev.xconfigure.y*(float)layer0.scn_win.height/(float)layer0.src_win.height-layer0.src_win.y+layer0.scn_win.y+(ev.xconfigure.height-height)/2;
+				layer.scn_win.width=((int)width)*layer0.scn_win.width/layer0.src_win.width;
+				layer.scn_win.height=((int)height)*layer0.scn_win.height/layer0.src_win.height;
+				ioctl(disp, DISP_CMD_LAYER_SET_PARA, args);
 				break;
 			case KeyPress:
-				keyevent(XKeycodeToKeysym(dis, ev.xkey.keycode,0));
+				keyevent(XKeycodeToKeysym(dis, ev.xkey.keycode, 0));
 				break;
-			//default: //printf("0x%X\n",ev.xkey.keycode);
 			case ClientMessage:
 				if(ev.xclient.data.l[0]== XWMDeleteMessage)keyevent('q');break;
 			case ButtonPress:
@@ -66,35 +86,38 @@ static void * eventthread()
 					case 5:keyevent('l');break;
 				}
 		}
+		cev.xclient.data.l[0]  = fs;
+		XSendEvent( dis, DefaultRootWindow(dis), 0, SubstructureRedirectMask | SubstructureNotifyMask, &cev );
 	}
+	return 0;
 }
+
 static void *inputthread()
 {
 	while(1)
 	{
 		keyevent(getchar());
 	}
+	return 0;
 }
 int main(int argc, char **argv)
 {
 	pthread_t thread_id;
-	__disp_fb_t fb;
 	int flag=1;
 	disp=open("/dev/disp",0);
 	char *s;
 	ioctl(disp,DISP_CMD_LAYER_GET_PARA,args);
 	if(layer0.mode!=DISP_LAYER_WORK_MODE_SCALER)layer0.src_win.width=layer0.src_win.height=layer0.scn_win.width=layer0.scn_win.height=1;
-	asprintf(&s,"/lib/ld-linux-armhf.so.3 --library-path . ./CedarXPlayerTest-1.4.1 %s", argv[1]);
-	pipe=popen(s,"w");
+	if(asprintf(&s,"/lib/ld-linux-armhf.so.3 --library-path . ./CedarXPlayerTest-1.4.1 %s", argv[1]) < 0)return 1;
+	cpt=popen(s,"w");
 	while(flag)
 	{
-		usleep(100000);
+		usleep(50000);
 		args[1]=102;
 		args[2]=(int)&layer;
 		if(flag)ioctl(disp,DISP_CMD_LAYER_GET_PARA,args);
-		//printf("\nlayer %d %d %d %d\n", hdl,ret, layer.scn_win.width, layer.scn_win.height);
 		layer.scn_win.width=layer.src_win.width;layer.scn_win.height=layer.src_win.height;
-		if(layer.scn_win.width&&layer.src_win.width&&flag)
+		if(layer.scn_win.width<4096&&layer.src_win.width<4096&&layer.scn_win.height<4096&&layer.src_win.height<4096&&layer.scn_win.width>0&&layer.src_win.width>0&&layer.scn_win.height>0&&layer.src_win.height>0&&flag)
 		{
 			flag=0;
 			layer.ck_enable=1;
@@ -107,7 +130,7 @@ int main(int argc, char **argv)
 			ck.red_match_rule = 2;
 			ck.green_match_rule = 2;
 			ck.blue_match_rule = 2;
-			args[1] = &ck;
+			args[1] = (int)&ck;
 			ioctl(disp, DISP_CMD_SET_COLORKEY, args);
 			args[1]=102;
 			ioctl(disp, DISP_CMD_LAYER_BOTTOM, args);
@@ -116,4 +139,5 @@ int main(int argc, char **argv)
 		}
 	}
 	wait(0);
+	return 0;
 }
